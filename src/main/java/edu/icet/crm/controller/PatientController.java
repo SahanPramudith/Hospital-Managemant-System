246941b79e1c05fbfb337d28826074736a815220
sahanpramudith <pramuditsahan466@gmail.com>
2024-10-13 15:27:44 +0530

    create  ResponseStatus

--- src/main/java/edu/icet/crm/controller/PatientController.java
+++ src/main/java/edu/icet/crm/controller/PatientController.java
@@ -3,6 +3,7 @@ package edu.icet.crm.controller;
 import edu.icet.crm.model.Patient;
 import edu.icet.crm.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class PatientController {
     PatientService service;
 
     @PostMapping("patient-save")
+    @ResponseStatus(HttpStatus.CREATED)
     void savePatien(@RequestBody Patient patient){
         service.addpatient(patient);
         System.out.println(patient);
@@ -26,10 +28,12 @@ public class PatientController {
     }
 
     @DeleteMapping("delete/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     void deletPatient(@PathVariable Integer id){
         service.deletepatient(id);
     }
     @PutMapping("/update")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     void updatepatient(@RequestBody Patient patient){
         service.addpatient(patient);
         System.out.println(patient);
